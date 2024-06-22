99f6810b64dd3886c840e621384f9914c42aa312
Stefan Schmaltz <info@checkboxit.de>
2024-06-22 14:00:26 +0200

    Reducing more memory 8->4 KB

--- src/WebSockets.h
+++ src/WebSockets.h
@@ -63,8 +63,8 @@
 
 #if defined(ESP8266) || defined(ESP32)
 
-#define WEBSOCKETS_MAX_DATA_SIZE (8 * 1024)
-#define WEBSOCKETS_USE_BIG_MEM
+#define WEBSOCKETS_MAX_DATA_SIZE (4 * 1024)
+//#define WEBSOCKETS_USE_BIG_MEM
 #define GET_FREE_HEAP ESP.getFreeHeap()
 // moves all Header strings to Flash (~300 Byte)
 // #define WEBSOCKETS_SAVE_RAM
